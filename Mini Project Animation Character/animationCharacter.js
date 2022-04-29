f3fd805f720211b4427fc2e25628364ca183c769
Htet Paing Kyaw <htetpaingk50@gmail.com>
2022-04-30 00:26:40 +0630

    Add Loading And Fix Code To Mini Project Animation Character

--- Mini Project Animation Character/animationCharacter.js
+++ Mini Project Animation Character/animationCharacter.js
@@ -5,28 +5,38 @@ let aa = '30 Days Of Javascript challeneg 2020  asabeneh yetayeh';
 
 let h = aa.split('');
 
-setInterval( () => {
-    f.innerText = '';
+function a(){
     let a = Math.random().toString(16).substring(2,8);
     let b = '#' + a;
 
     box.style.backgroundColor = b;
 
-    for(let i = 0;i < h.length;i++){
+    for(j = 0;j < h.length;j++){
         let d = Math.random().toString(16).substring(2,8);
         let e = '#' + d;
         let g = document.createElement('span');
         
-        g.innerText = h[i];
+        g.innerText = h[j];
         g.style.color = e;
-        g.style.fontSize = '75px';
-        g.style.textTransform = 'uppercase'
-        g.style.letterSpacing = '10px';
-        
-        f.appendChild(g);
-    }
+        g.classList.add('text');
 
-},1000)
+        f.appendChild(g);
+    };
+};
+
+a();
+
+setInterval( () => {
+    f.innerText = '';
+    a();
+
+},2000);
 
 
 box.appendChild(f);
+
+$(window).on("load",function(){
+    $(".loader-container").fadeOut(500,function(){
+        $(this).remove();
+    });
+});
